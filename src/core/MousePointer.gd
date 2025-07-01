b4f39ee566a6a0ad7a4648bd5030404e4381f17e
menaechmi <menaechmi+dev@protonmail.com>
2025-07-01 15:21:37 -0500

    MousePointer: re-enable offset_mouse_position
    Not sure why I removed it in the first place

--- src/core/MousePointer.gd
+++ src/core/MousePointer.gd
@@ -124,7 +124,7 @@ func determine_global_mouse_pos() -> Vector2:
 	#var scaling_offset = get_tree().get_root().get_node('Main').get_viewport().get_size_override() * OS.window_size
 	if cfc.ut and cfc.NMAP.get("board"):
 		mouse_position = cfc.NMAP.board._UT_mouse_position
-	else: mouse_position = get_tree().get_root().get_mouse_position()
+	else: mouse_position = offset_mouse_position
 	return mouse_position
 
 
