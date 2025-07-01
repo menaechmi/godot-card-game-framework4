16d5df9b91a94532c1accb3c9c16761b0ba09d0c
menaechmi <menaechmi+dev@protonmail.com>
2025-06-30 21:39:43 -0500

    fix order of tests (got, expected) from (expected, got)

--- tests/integration/test_manipulation_buttons.gd
+++ tests/integration/test_manipulation_buttons.gd
@@ -13,7 +13,7 @@ class TestManipulationButtons:
 		await move_mouse(card.global_position - Vector2(0,100))
 		await yield_for(0.2)
 		await move_mouse(card.global_position)
-		assert_almost_eq(Vector2(103, -240.5),cards[0].position,Vector2(2,2),
+		assert_almost_eq(cards[0].position, Vector2(103, -240.5), Vector2(2,2),
 				"Card focused and in correct global position")
-		assert_almost_eq(Vector2(1.5, 1.5),cards[0].scale,Vector2(0.1,0.1),
+		assert_almost_eq(cards[0].scale, Vector2(1.5, 1.5), Vector2(0.1,0.1),
 				"Card has correct scale")
