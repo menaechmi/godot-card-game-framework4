d6948426c15e934b5fde133e8a5ae862e0f62fb5
menaechmi <menaechmi+dev@protonmail.com>
2025-07-17 11:52:16 -0500

    [Tests] Fix test_targetting

--- tests/integration/test_targetting.gd
+++ tests/integration/test_targetting.gd
@@ -19,18 +19,19 @@ func test_targetting():
 	var card: Card
 	card = cards[0]
 	card.targeting_arrow.initiate_targeting()
+	#First we target card[4] in the hand
 	await move_mouse(cards[4].global_position)
 	await wait_frames(20)
 	assert_lt(0,card.targeting_arrow.get_point_count(),
 			"test that TargetLine has length higher than 1 while active")
 	assert_true(card.targeting_arrow.get_node("ArrowHead").visible,
 			"test that arrowhead is visible on once active")
-	#TODO: check positioning of mouse
 	assert_true(cards[4].highlight.visible,
 			"Test that a target arrow hovering over another card, highlights it")
 	assert_eq(cards[4].highlight.modulate, CFConst.TARGET_HOVER_COLOUR,
 			"Test that a hovered target has the right colour highlight")
 	card.targeting_arrow.complete_targeting()
+	await wait_frames(20)
 	assert_eq(card.targeting_arrow.target_object,cards[4],
 			"Test that card in hand can target card in hand")
 	assert_eq(0,card.targeting_arrow.get_point_count(),
@@ -41,11 +42,15 @@ func test_targetting():
 	assert_false(cards[4].highlight.visible,
 			"Test that a highlights disappears once targetting ends")
 
+	#Now we move two cards to the table, overlapping
 	await table_move(cards[3],Vector2(300,300))
 	await table_move(cards[2],Vector2(350,400))
+	#Instead of basing this on timing, we average the x and y of both cards
+	await move_mouse(Vector2(0,0)) #Card will still be highlighted if the mouse is on it
 	card.targeting_arrow.initiate_targeting()
+	#Because cards[2] position (top left) overlaps with cards[3], we target it
 	board._UT_interpolate_mouse_move(cards[2].global_position,card.global_position,3)
-	await wait_seconds(1)
+	await wait_seconds(0.5) #For mouse movement to finish
 	assert_true(cards[2].highlight.visible,
 			"test that hovering over multiple cards selects the top one")
 	assert_false(cards[3].highlight.visible,
@@ -54,6 +59,7 @@ func test_targetting():
 	assert_eq(card.targeting_arrow.target_object,cards[2],
 			"Test that card in hand can target card on board")
 
+	#Now we test targeting from our card on the board - should probably be a separate test
 	card = cards[2]
 	card.targeting_arrow.initiate_targeting()
 	board._UT_interpolate_mouse_move(cards[3].global_position,card.global_position,3)
